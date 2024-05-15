6ed6900a4c6413a72c0c6317b75bbd4e43db2320
huydlpm <huydl281098@gmail.com>
2024-05-15 17:27:31 +0700

    Study - Work with styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Person from "./components/Person/Person";
+import Example from "./components/Example";
+import { useState } from "react";
 
 function App() {
+  const [person, setPersons] = useState([
+    { name: "Đặng Lê Huy", age: 26 },
+    { name: "Đặng Hải An", age: 20 },
+  ]);
+
+  const changePersonName = (e) => {
+    setPersons([
+      { name: "Đặng Lê Huy 1", age: 26 },
+      { name: "Đặng Hải An 1", age: 20 },
+    ]);
+  };
+
+  const changeName = (e) => {
+    setPersons([
+      { name: e.target.value, age: 26 },
+      { name: "Đặng Hải An 1", age: 20 },
+    ]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Wellcome, Create react app!</h3>
+      <button onClick={() => changePersonName()}>Change Name</button>
+      <p>This is a person</p>
+      <Person
+        click={changePersonName}
+        name={person[0].name}
+        age={person[0].age}
+        changeTxt={changeName}
+      >
+        My hobbies is football
+      </Person>
+      <Person
+        click={changePersonName}
+        name={person[1].name}
+        age={person[1].age}
+        changeTxt={changeName}
+      />
+      <Example />
     </div>
   );
 }
